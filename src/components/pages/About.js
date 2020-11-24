15f92ab2c5e242407ef87ed17fa4bfaaaa68d296
AbbasGhayth <abbasghayth5@gmail.com>
2020-11-25 01:12:43 +0200

    github finder project

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>About This App</h1>
+            <p>App to search Github users</p>
+            <p>Author: Abbas Ghaith</p>
+            <p>Email: abbasg@microtech.solutions</p>
+            <p>Phone: +961 76 77 53 65</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>
+    )
+}
+
+export default About;
